4eccf0b4a26bc4ea6f6c8786aee1b591b6ece159
NikinikitaN <119474174+NikinikitaN@users.noreply.github.com>
2023-09-22 18:51:10 +0530

    Update interact.ts

--- scripts/interact.ts
+++ scripts/interact.ts
@@ -2,10 +2,10 @@ import dotenv from "dotenv";
 dotenv.config(); // Load environment variables from .env file
 import { ethers } from "ethers";
 const hre = require("hardhat");
-const telecomSharingJSON = require("./../artifacts/contracts/TelecomInfrastructureSharing.sol/TelecomSharing.json");
+const TelecomSharingJSON = require("./../artifacts/contracts/TelecomInfrastructureSharing.sol/TelecomSharing.json");
 
 const contractAddress: string = process.env.CONTRACT_ADDRESS || "";
-const contractABI: any[] = telecomSharingJSON.abi;
+const contractABI: any[] = TelecomSharingJSON.abi;
 
 async function main() {
   const provider = new ethers.JsonRpcProvider(process.env.RPC_NODE_URL || "");
@@ -14,24 +14,18 @@ async function main() {
 
   try {
     // Call functions here
-    const createAccountTx = await contract.createAccount();
-    await createAccountTx.wait();
 
-    console.log("Account Created", signer.getAddress());
-    const accountBalance = await contract.accounts(signer.getAddress());
-    console.log("Account Balance:", accountBalance.toString()); // Convert BigNumber to string
+//    function createProduct(string memory _productName, string memory _productDescription, uint _productValue) public {
+  const createproduct = await contract.shareInfrastructure();
+  await createproduct.wait();
+  console.log("product created");
+  console.log("The transaction hash is:", createproduct.hash);
+  const receipt = await createproduct.wait();
+  console.log(
+    "The transaction returned the following transaction receipt:\n",
+    receipt,
+  );
 
-    const accountCount = await contract.getAccountCount();
-    console.log("Account Count:", accountCount.toString()); // Convert BigNumber to string
-
-    const accountAddresses = [];
-    for (let i = 0; i < accountCount; i++) {
-      const addressAtIndex = await contract.getAccountAtIndex(i);
-      accountAddresses.push(addressAtIndex);
-    }
-    console.log("Account Addresses:", accountAddresses);
-    console.log("Check your transaction on BBN Testnet:", `http://testnet.bharatblockchain.io/address/${contractAddress}`);
-  
 
   } catch (error) {
     console.error("Error:", error);
